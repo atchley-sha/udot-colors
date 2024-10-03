1fa8f58b8e06f809dab9e9a068365a07c9e9b180
Hayden Atchley <hatchley@utah.gov>
2024-10-02 18:35:50 -0600

    Update plotnine scales

--- udotcolors/plotnine/palettes.py
+++ udotcolors/plotnine/palettes.py
@@ -1,9 +1,9 @@
-from ..colors import get_color, get_palette
-import matplotlib as mpl
+from ..colors import get_color
+from ..palettes import get_palette_qual, get_palette_seq, get_palette_div
+import matplotlib.colors as mcolors
 import plotnine
 import ColorKit as ck
 import pprint
-import warnings
 
 
 def _palette_gen(
@@ -12,15 +12,49 @@ def _palette_gen(
     # backward=False,
 ):
     def f(n):
-        if n > len(get_palette(palette)):
-            warnings.warn("Not enough colors in this palette!")
+        if n > len(get_palette_qual(palette)):
+            raise Exception(
+                "Not enough colors in this palette!\n"
+                + 'Number of colors in palette "'
+                + palette
+                + '": '
+                + str(len(get_palette_qual(palette)))
+                + "\n"
+                + "Number of colors asked for: "
+                + str(n)
+            )
+
+        colors = get_palette_qual(palette)
+        # if backward:
+        #     colors.reverse()
+        pal = colors[:n]
+        if reverse:
+            pal.reverse()
+        return pal
+
+    return f
+
+
+def _palette_gen_seq(palette=None, colorlist=None, reverse=False):
+
+    map_name = palette if colorlist is None else "colorlist"
+
+    if palette is not None and colorlist is not None:
+        raise UserWarning(
+            "Both `palette` and `colorlist` specified. Will use `colorlist`."
+        )
+
+    if colorlist is None:
+        if palette is None:
+            raise ValueError("Specify one of `palette` or `colorlist`")
         else:
-            colors = get_palette(palette)
-            # if backward:
-            #     colors.reverse()
-            pal = colors[:n]
-            if reverse:
-                pal.reverse()
-            return pal
+            colorlist = get_palette_seq(palette)
+
+    if reverse:
+        colorlist.reverse()
+
+    def f():
+        pal = mcolors.LinearSegmentedColormap.from_list(map_name, colorlist)
+        return pal
 
     return f
--- udotcolors/plotnine/scales.py
+++ udotcolors/plotnine/scales.py
@@ -1,11 +1,32 @@
 import plotnine
-from .palettes import _palette_gen
+from .palettes import _palette_gen, _palette_gen_seq
+
+## Discrete scales ####
+
+
+def scale_fill_udot(
+    palette="main",
+    reverse=False,
+    # backward=False,
+    **kwargs,
+):
+    scale = plotnine.scales.scale_discrete.scale_discrete(
+        aesthetics="fill",
+        palette=_palette_gen(
+            palette=palette,
+            reverse=reverse,
+            # backward=backward,
+        ),
+        **kwargs,
+    )
+    return scale
 
 
 def scale_color_udot(
     palette="main",
     reverse=False,
     # backward=False,
+    **kwargs,
 ):
     scale = plotnine.scales.scale_discrete.scale_discrete(
         aesthetics="color",
@@ -14,8 +35,39 @@ def scale_color_udot(
             reverse=reverse,
             # backward=backward,
         ),
+        **kwargs,
     )
     return scale
 
 
 scale_colour_udot = scale_color_udot
+
+
+## Sequential scales ####
+
+
+def scale_fill_udot_seq(
+    palette=None,
+    colorlist=None,
+    reverse=False,
+    **kwargs,
+):
+    pal = _palette_gen_seq(palette=palette, colorlist=colorlist, reverse=reverse)
+    scale = plotnine.scale_fill_gradientn(
+        colors=pal()([x / 256 for x in range(257)]),
+        **kwargs,
+    )
+    return scale
+
+
+# def scale_color_udot_seq()
+
+# scale_colour_udot_seq = scale_color_udot_seq
+
+
+## Diverging scales
+
+# def scale_fill_udot_div()
+# def scale_color_udot_div()
+
+# scale_colour_udot_div = scale_color_udot_div
